3ceadfb639941daacad4d064451dbc0368bf2f36
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2017-11-11 17:35:55 +0100

    Added debug strings

--- conanfile.py
+++ conanfile.py
@@ -39,6 +39,7 @@ conan_basic_setup()''')
             if self.options.fPIC:
                 flags.append("-fPIC")
 
+        print("arch: {0} flags {1}".format(self.settings.arch, flags))
         if compiler in ("clang", "apple-clang"):
             # without the following, compilation gets stuck indefinitely
             flags.append("-Wno-deprecated-declarations")
