3aa27f1105566c50d78ef2bb76f76d5fae47985c
Dariusz Ostolski <dariusz.ostolski@gmail.com>
2017-11-11 18:07:40 +0100

    Added options for test package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,7 +9,23 @@ class GflagsTestConan(ConanFile):
     def build(self):
         cmake = CMake(self)
         cmake.verbose = True
+        compiler = str(self.settings.compiler)
+        flags = []
+
+        if compiler in ("gcc", "clang", "apple-clang"):
+            if self.settings.arch == 'x86':
+                flags.append("-m32")
+            else:
+                flags.append("-m64")
+
+            if self.options.fPIC:
+                flags.append("-fPIC")
+
         self.output.info("arch: {0}".format(self.settings.arch))
+
+        cmake.definitions["CMAKE_C_FLAGS"] = " ".join(flags)
+        cmake.definitions["CMAKE_CXX_FLAGS"] = cmake.definitions["CMAKE_C_FLAGS"]
+        
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
         cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
         cmake.build()
