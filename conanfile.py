21ab152265c591f06ed12c2fddef366a11fa7afd
Croydon <cr0ydon@outlook.com>
2019-04-10 00:16:19 +0200

    Apply conventions updates (close #7)

--- conanfile.py
+++ conanfile.py
@@ -8,27 +8,32 @@ import os
 class GflagsConan(ConanFile):
     name = "gflags"
     version = "2.2.1"
-    description = "The gflags package contains a C++ library that implements commandline flags processing. "
+    description = "The gflags package contains a C++ library that implements commandline flags processing"
+    topics = ("conan", "gflags", "cli", "flags")
     url = "https://github.com/bincrafters/conan-gflags"
-    license = 'BSD 3-clause'
+    homepage = "https://github.com/gflags/gflags"
+    license = 'BSD-3-Clause'
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "Findgflags.cmake"]
-    source_subfolder = "source_subfolder"
     generators = "cmake"
+
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False], "nothreads": [True, False], "namespace": "ANY"}
-    default_options = "shared=False", "fPIC=True", "nothreads=True", "namespace=gflags"
+    default_options = {'shared': False, 'fPIC': True, 'nothreads': True, 'namespace': 'gflags'}
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def configure(self):
         if self.settings.os == "Windows":
             self.options.remove("fPIC")
 
     def source(self):
-        source_url = "https://github.com/gflags/gflags"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
-        os.rename("%s-%s" % (self.name, self.version), self.source_subfolder)
+        sha256 = "ae27cdbcd6a2f935baa78e4f21f675649271634c092b1be01469440495609d0e"
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
+        os.rename("%s-%s" % (self.name, self.version), self._source_subfolder)
 
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
         cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
@@ -42,14 +47,21 @@ class GflagsConan(ConanFile):
         cmake.definitions["REGISTER_BUILD_DIR"] = False
         cmake.definitions["REGISTER_INSTALL_PREFIX"] = False
         cmake.definitions["GFLAGS_NAMESPACE"] = self.options.namespace
+        cmake.configure(build_folder=self._build_subfolder)
+        return cmake
 
-        cmake.configure()
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
+
 
     def package(self):
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
         self.copy("Findgflags.cmake", ".", ".")
-        self.copy("COPYING.txt", dst="licenses", src=self.source_subfolder)
+        self.copy("COPYING.txt", dst="licenses", src=self._source_subfolder)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
