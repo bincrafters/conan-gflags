3188e42d9b812d9b42663289e547203ff8673b4a
Uilian Ries <uilianries@gmail.com>
2019-04-12 09:54:30 -0300

    Distribute gflags with threads
    - Remove custom Findgflags.cmake. Conan generator cmake_find_package
      should be enough.
    - Add libm on Linux. ldd reveals pthread and libm as requirements
    - Build with extra option
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from bincrafters import build_template_default
+from copy import deepcopy
+
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder(pure_c=False)
-
+    builds = list(builder.items)
+    thread_builds = deepcopy(builds)
+    for build in thread_builds:
+        build.options['gflags:nothreads'] = False
+    builder.items.extend(thread_builds)
     builder.run()
